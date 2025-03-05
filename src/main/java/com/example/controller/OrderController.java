150da6eeaa0541f5dc85a34146f9483d392b7335
Farahat <118939263+ahmed-farahat-pro@users.noreply.github.com>
2025-03-05 20:20:42 +0200

    Update OrderController.java

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.model.Order;
 import com.example.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,8 +12,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-    private final OrderService orderService;
-
+     OrderService orderService;
+    @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
